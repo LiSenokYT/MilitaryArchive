daa55246ead56478053d4fe0e30544ad9c48c52d
LiSenokYT <131493918+LiSenokYT@users.noreply.github.com>
2025-12-15 02:13:37 +0300

    Delete js directory

--- js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
-
-const supabaseUrl = 'https://nbqoepiouxikzganqodg.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5icW9lcGlvdXhpa3pnYW5xb2RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3ODQwNTcsImV4cCI6MjA3ODM2MDA1N30.iJY3DYuLPSLArZx6zVTXoowW58HgJT5R3dNIZLNsrIo'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
